7c9946b38c16f33fbbdb83b619d42de8927b6b5c
pierobramanti <piero.bramanti94@gmail.com>
2024-10-12 11:23:07 +0200

    [UPD] filled Database with TypeSeeder seeders

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type; 
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Frontend', 'Backend', 'Fullstack', 'Database', 'Design', 'UI/UX Design'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+
+            $new_type->name = $type;
+            $new_type->slug = Type::generateSlug($type); 
+
+            $new_type->save(); 
+        }
     }
 }
